ef2de7e48d882d7029e9748da087d012f776a70a
GeoRPOliveira <geovanna.oliveira@germinare.org.br>
2025-03-18 10:34:58 -0300

    Fron home finalizado

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,18 @@ import Header from './Components/Header/Header.jsx'
 import Footer from './Components/Footer/Footer.jsx'
 import MaisVendidos from './Components/MaisVendidos/MaisVendidos.jsx'
 import Button from './Components/Button/Button.jsx'
+import style from './main.module.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
-    <section>
-      <div>
-        <h1></h1>
-        <h3></h3>
+    <section className={style.principal}>
+      <div className={style.divTextos}>
+        <h1 className={style.titulo}>Doceria caseira</h1>
+        <h3 className={style.descricao}>Descubra nossas tortas, cookies, bolos e muito mais! Tudo feito a mão e com os melhores ingredientes!</h3>
         <Button titulo={"Compre agora"} backgroundColor="rosa"/>
       </div>
-      <img src="" alt="" />
+      <img className={style.imagem} src="https://i.pinimg.com/736x/50/df/6a/50df6a7fa81f0964c8a1cb81e8d81f45.jpg" alt="" />
     </section>
     <MaisVendidos />
     <Footer />
